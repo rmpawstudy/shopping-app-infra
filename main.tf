c3713e10e913c4d2ae159e55e3bae85fa3c49d95
rmp <rmp@gmail.com>
2023-12-28 18:20:20 +0000

    infra dev version1

--- main.tf
+++ main.tf
@@ -35,16 +35,6 @@ resource "aws_security_group" "http_access" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
-  #PROMETHUS MONITORING
-
-  ingress {
-    description      = "PROMETHEUS"
-    from_port        = 9090
-    to_port          = 9090
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
 
   egress {
     from_port        = 0
